fd1c58a3fd5b525c350baa8ccfc428a353a8ee89
Forrest Jablonski <highoffscience@gmail.com>
2022-11-16 22:56:29 -0500

    ip

--- common/test_main.cpp
+++ common/test_main.cpp
@@ -6,13 +6,38 @@
 
 #include <cstdint>
 #include <iostream>
+#include <bit>
+#include <bitset>
 
 #include "ym.h"
 #include "random.h"
 
+/*
+
+0000 - 0
+0001 - 1
+0010 - 1
+0011 - 2
+0100 - 1
+0101 - 2
+0110 - 2
+0111 - 3
+1000 - 1
+1001 - 2
+1010 - 2
+1011 - 3
+1100 - 2
+1101 - 3
+1110 - 3
+1111 - 4
+
+*/
+
 int main(void)
 {
    using namespace ym;
+
+   auto b = std::bitset<64>(127ull);
 
    auto r = Random();
    
