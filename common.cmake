8b762a00ebf890523fad46f3b589060ed51bfe1b
Forrest Jablonski <highoffscience@gmail.com>
2022-12-22 15:14:24 -0500

    ip

---
+++ common.cmake
@@ -0,0 +1,18 @@
+##
+# @file    common.cmake
+# @version 1.0.0
+# @author  Forrest Jablonski
+#
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+
+set(RootDir     ${CMAKE_CURRENT_LIST_DIR})
+set(CommonDir   ${RootDir}/common/)
+set(FmtDir      ${RootDir}/fmt/)
+
+set(CompileFlags
+   -Werror
+   -Wall
+   -Wextra
+   -O2)
--- unittests/utcommon.cmake
+++ unittests/utcommon.cmake
@@ -1,16 +1,12 @@
 ##
-# @file    common.cmake
+# @file    utcommon.cmake
 # @version 1.0.0
 # @author  Forrest Jablonski
 #
 
-set(CMAKE_CXX_STANDARD 20)
-set(CMAKE_CXX_STANDARD_REQUIRED True)
+include(../common.cmake)
 
-set(RootDir     ${CMAKE_CURRENT_LIST_DIR})
-set(CommonDir   ${RootDir}/common/)
 set(UnitTestDir ${RootDir}/unittests/)
-set(FmtDir      ${RootDir}/fmt/)
 
 set(UTCommonSrcs
    ${UnitTestDir}/common/datashuttle.cpp
@@ -18,9 +14,6 @@ set(UTCommonSrcs
    ${UnitTestDir}/common/testcase.cpp
    ${UnitTestDir}/common/testsuitebase.cpp)
 
-set(CompileFlags
-   -Werror
-   -Wall
-   -Wextra
-   -O2
+set( CompileFlags
+   ${CompileFlags}
    -DFMT_HEADER_ONLY)
