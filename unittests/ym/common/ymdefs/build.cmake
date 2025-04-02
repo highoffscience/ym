61df323421e216c734b5e65b49b15d356b42f9bf
Forrest Jablonski <highoffscience@gmail.com>
2025-04-01 22:45:31 -0400

    DRAFT

--- unittests/ym/common/ymdefs/build.cmake
+++ unittests/ym/common/ymdefs/build.cmake
@@ -10,9 +10,17 @@
 #
 function(ym.common.ymdefs Ctx_JSON)
 
-   set(Target ${CMAKE_CURRENT_FUNCTION}.unittest)
+   set(Target    ${CMAKE_CURRENT_FUNCTION})
+   set(TargetAll ${CMAKE_CURRENT_FUNCTION}-all)
+   set(TargetRun ${CMAKE_CURRENT_FUNCTION}-run)
+
    string(REPLACE "." "/" RelPath ${CMAKE_CURRENT_FUNCTION})
+
    add_library(${Target} SHARED)
+
+   add_custom_target(${TargetAll} DEPENDS ${Target})
+   add_custom_target(${TargetRun} DEPENDS ${Target})
+
    target_sources(${Target} ${CMAKE_SOURCE_DIR}/${RelPath}/testsuite.cpp)
    target_link_libraries(${Target} PRIVATE ym.common)
 
