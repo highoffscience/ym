a84fa5773bfc3a1c45dd8d0d2d31964d79d267d8
Forrest Jablonski <highoffscience@gmail.com>
2025-04-08 23:02:53 -0400

    DRAFT

--- unittests/ym/build.cmake
+++ unittests/ym/build.cmake
@@ -11,23 +11,21 @@ cmake_minimum_required(VERSION 3.27)
 # @brief Defines target to build all child unittests.
 #
 function(ym Ctx_JSON)
-   set(SubBuilds common)
+
    set(Target    ${CMAKE_CURRENT_FUNCTION})
    set(TargetAll ${Target}-unittests)
    set(TargetRun ${Target}-run)
+   set(TargetInt ${Target}-interface)
 
    add_custom_target(${TargetAll})
    add_custom_target(${TargetRun})
 
-   foreach(SubBuild ${SubBuilds})
-      set(SubTarget    ${Target}.${SubBuild})
-      set(SubTargetAll ${SubTarget}-unittests)
-      set(SubTargetRun ${SubTarget}-run)
+   add_library(${TargetInt} INTERFACE)
 
-      include(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/${SubBuild}/build.cmake)
-      cmake_language(CALL ${SubTarget} Ctx_JSON)
-
-      add_dependencies(${TargetAll} ${SubTargetAll})
-      add_dependencies(${TargetRun} ${SubTargetRun})
+   set(SubTargets common)
+   foreach(SubTarget ${SubTargets})
+      include(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/${SubTarget}/build.cmake)
+      cmake_language(CALL ${Target}.${SubTarget} Ctx_JSON)
    endforeach()
+   
 endfunction()
