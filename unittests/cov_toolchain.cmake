9db793281c7eed9699375089bba85ec39299f07d
Forrest Jablonski <highoffscience@gmail.com>
2025-04-15 23:09:01 -0400

    DRAFT

--- unittests/cov_toolchain.cmake
+++ unittests/cov_toolchain.cmake
@@ -11,7 +11,7 @@ set(CMAKE_ASM_COMPILER /usr/bin/clang)
 set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED True)
 
-list(APPEND CMAKE_CXX_FLAGS -fprofile-instr-generate -fcoverage-mapping)
-list(APPEND CMAKE_C_FLAGS   -fprofile-instr-generate -fcoverage-mapping)
-
-list(APPEND CMAKE_SHARED_LINKER_FLAGS -fprofile-instr-generate -fcoverage-mapping)
+set(YM_CovFlags -fprofile-instr-generate -fcoverage-mapping)
+list(APPEND CMAKE_CXX_FLAGS           ${YM_CovFlags})
+list(APPEND CMAKE_C_FLAGS             ${YM_CovFlags})
+list(APPEND CMAKE_SHARED_LINKER_FLAGS ${YM_CovFlags})
